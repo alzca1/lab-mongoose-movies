0a6fb9eeb3e1a8fa6053ddb47abb03e5c46aee23
Alvaro Zajara <alzca1@gmail.com>
2019-07-23 15:07:31 +0200

    5th iteration

--- routes/celebrities.js
+++ routes/celebrities.js
@@ -13,6 +13,20 @@ router.get('/', async (req, res, next) => {
   }
 });
 
+router.get('/create', (req, res, next) => {
+  res.render('celebrities/new');
+});
+
+router.post('/create', async (req, res, next) => {
+  try {
+    const celebrity = req.body;
+    await Celebrity.create(celebrity);
+    res.redirect('/celebrities');
+  } catch (error) {
+    next(error);
+  }
+});
+
 router.get('/:id', async (req, res, next) => {
   try {
     const { id } = req.params;
